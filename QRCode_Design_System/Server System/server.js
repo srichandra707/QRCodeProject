e1250a543beb439d0e67edcee23ce530188eb425
Srichandra Lolla <129283037+srichandra707@users.noreply.github.com>
2023-05-16 00:16:02 +0530

    added a note next to users variable which needs to be changed to a database later

--- QRCode_Design_System/Server System/server.js
+++ QRCode_Design_System/Server System/server.js
@@ -4,6 +4,6 @@ const cors = require('cors');
 const port = process.env.API_PORT || 5665;
 const bcrypt = require('bcrypt');
 const saltRounds = 5;
-const users = require('./data').users;
+const users = require('./data').users; //here its made to work in a file instead of a database, need to change it later.
 server.use(express.json());
 server.use(cors());
